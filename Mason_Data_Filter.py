685250747253be631d0659cffcb3cf2790c1c327
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2021-06-14 19:51:22 -0500

    Update Mason_Data_Filter.py

--- Mason_Data_Filter.py
+++ Mason_Data_Filter.py
@@ -6,7 +6,7 @@ import pandas as pd
 df_gene_tpm = pd.read_csv("results_TPM_gene.tsv",sep='\t')
 
 
-#####Filter TPM
+
 #create a median column
 df_gene_tpm['median']=df_gene_tpm.median(axis=1)
 #Remove genes where TPM median < 1, except for SARs genes. 
@@ -18,7 +18,7 @@ med_med_pc=df_gene_tpm.loc[df_gene_tpm['Gene_type'] == 'protein_coding']['median
 med_med_lnc=df_gene_tpm.loc[df_gene_tpm['Gene_type'] == 'lncRNA']['median'].median()
 med_med_eb=df_gene_tpm.loc[df_gene_tpm['Gene_type'] == 'EB_novel']['median'].median()
 #Remove EB genes where median TPM is less than that of the med_med_lnc
-indexNames = df_gene_tpm[(df_gene_tpm['Gene_type'] == 'EB_novel') & (df_gene_tpm['median'] < med_med_lnc) ].index
-df_gene_tpm.drop(indexNames , inplace=True)
+#indexNames = df_gene_tpm[(df_gene_tpm['Gene_type'] == 'EB_novel') & (df_gene_tpm['median'] < med_med_lnc) ].index
+#df_gene_tpm.drop(indexNames , inplace=True)
 
 df_gene_tpm.to_csv('JH-2021610-tpm.filtered-Mason-Nasal',sep='\t',index=False)        
