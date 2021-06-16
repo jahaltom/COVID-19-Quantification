309be3b11db6ae44d3e79b2e7862976efccfdad4
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2021-06-16 09:57:50 -0500

    Update and rename make_fasta's to make_fasta.sh

--- Metadata/make_fasta.sh
+++ Metadata/make_fasta.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 awk '{if(NR==1) {print $0} else {if($0 ~ /^>/) {print "\n"$0} else {printf $0}}}' EB_Gencode_pep_NODOTS.fasta | sed 'N;s/\n/ /' | perl -p -i -e 's/ /\t/g' | grep "|ENSG0" > human
 cat human | sed 's/|/ /g' | awk 'BEGIN {OFS = "\t"}{print $2,$NF}' > Human
 rm human
