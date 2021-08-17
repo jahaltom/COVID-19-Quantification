505b2e86216fdfbb4680187e6cfeefaeb5180dd8
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2021-08-17 18:52:31 -0500

    Update Bam_Quant_Filter.py

--- Bam_Quant_Filter.py
+++ Bam_Quant_Filter.py
@@ -14,9 +14,9 @@ indexNames = df_gene_tpm[(df_gene_tpm['median'] < 1) & (df_gene_tpm['Gene_type']
 df_gene_tpm.drop(indexNames , inplace=True)
 
 #calculate medians of median tpm dist for protein_coding, lncRNA, and EB genes
-med_med_pc=df_gene_tpm.loc[df_gene_tpm['Gene_type'] == 'protein_coding']['median'].median()
-med_med_lnc=df_gene_tpm.loc[df_gene_tpm['Gene_type'] == 'lncRNA']['median'].median()
-med_med_eb=df_gene_tpm.loc[df_gene_tpm['Gene_type'] == 'EB_novel']['median'].median()
+#med_med_pc=df_gene_tpm.loc[df_gene_tpm['Gene_type'] == 'protein_coding']['median'].median()
+#med_med_lnc=df_gene_tpm.loc[df_gene_tpm['Gene_type'] == 'lncRNA']['median'].median()
+#med_med_eb=df_gene_tpm.loc[df_gene_tpm['Gene_type'] == 'EB_novel']['median'].median()
 #Remove EB genes where median TPM is less than that of the med_med_lnc
 #indexNames = df_gene_tpm[(df_gene_tpm['Gene_type'] == 'EB_novel') & (df_gene_tpm['median'] < med_med_lnc) ].index
 #df_gene_tpm.drop(indexNames , inplace=True)
