d214e79d60a57bf98fcc1ae14f645aa6b6afdda7
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2023-03-21 14:22:48 -0400

    Update DESeq2.r

--- DESeq2.r
+++ DESeq2.r
@@ -1,42 +1,39 @@
+library("tximport")
 library(DESeq2)
-library(dplyr)
-
-#Read in count information.
-countData = read.table("results_Count_gene.filtered.tsv",header=TRUE,sep = '\t',quote="",check.names=FALSE)
-
-#X an Y gene names can be the same. This keeps 1st occurance.
-countData <- countData[!duplicated(countData$Gene_stable_ID),]
-rownames(countData)= countData$Gene_stable_ID
 
 
-#Select only columns that contain counts.
-countData=select(countData,contains("SRR"))
+#Make tx2gene file
+txMD = read.table("Transcript_level_metadata.tsv",header=TRUE,sep = '\t')
+tx2gene=data.frame(txMD$TranscriptID,txMD$Gene_ID_ver)
 
-#Round to nearest int
-countData=round(countData,0)
+#List quant.sf files
+samples = read.table("ids.txt",header=FALSE,sep = '\t')
+files <- file.path("out", samples$V1, "salmon_out", "quant.sf")
+names(files) <- paste0(samples$V1)
 
-##Read in expermental design and gene metadata. rownames is SampleID.
-metadata = read.table("metadata.tsv",header=TRUE,row.names=1,sep = '\t',quote="")
-gene_metadata = read.table("Gene_level_metadata.tsv",header=TRUE,sep = '\t',quote="")
+#tximport
+txi.salmon <- tximport(files, type = "salmon", tx2gene = tx2gene)
+
+#Read in metadata
+md = read.table("metadata.tsv",header=TRUE,sep = '\t',row.names=2)
+#Reorder metadata rows to match count data col
+md=md[colnames(txi.salmon$counts),]
+#Make DeSeq object
+dds <- DESeqDataSetFromTximport(txi.salmon,colData= md, design = ~Condition)
+
+##Filter DeSeq object
+filteredGeneList = read.table("results_Count_gene.filtered.tsv",header=TRUE,sep = '\t')
+#Obtain the indices of only desired genes
+genesTokeep <- which(rownames(dds) %in% filteredGeneList$Gene_ID_ver)
+#extract your desired genes in the DESeq object
+dds <- dds[genesTokeep, ]
+
+##DESeq2
 
 
-
-#Reorder count columns to match metadata
-countData=countData[,rownames(metadata)]
-
-
-#Should return TRUE
-#all(rownames(metadata) == colnames(countData))
-
-
-
-
-
-##############################EB
-##Make DEseq2 object
-dds = DESeqDataSetFromMatrix(countData = countData,colData = metadata,design = ~ Condition)
 dds = DESeq(dds)
 
+gene_metadata = read.table("Gene_level_metadata.tsv",header=TRUE,sep = '\t',quote="")
 
 
 list = read.table("Combo.tsv",sep = '\t',quote="")
@@ -48,11 +45,8 @@ for (x in 1:nrow(list)) {
     ## Remove rows with NA
     result = result[complete.cases(result),]
     #Put GeneID as column
-    result = cbind(Gene_stable_ID = rownames(result), result)
-    result=merge(gene_metadata,result,by="Gene_stable_ID",all.y=TRUE)
+    result = cbind(Gene_ID_ver = rownames(result), result)
+    result=merge(gene_metadata,result,by="Gene_ID_ver",all.y=TRUE)
     write.table(result,paste(list[x,1],"vs",list[x,2],".DGE.tsv",sep='_') ,sep = '\t',row.names = FALSE)
 
 }
-
-
-
