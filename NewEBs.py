1488b7d045947ed691b11f3b62b6dd2d94f5fadc
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2023-07-25 21:51:48 -0400

    Update NewEBs.py

--- NewEBs.py
+++ NewEBs.py
@@ -1,28 +1,21 @@
 import pandas as pd
+from pandas import DataFrame
 import glob
 
-
-
-
-
-
 #Will appened Gene_stable_IDs for each combonation
 combos=[]
 
 #Files that contain the EB genes that passed each combonation filter.
-files=glob.glob("*_Covid_*")
+files=glob.glob("*covid_*")
 
 for i in files:
     df=pd.read_csv(i,sep='\t')
     #Sample # must be >= 5 and be EB
-    if (len([col for col in df if col.startswith('batch')])) >=5:
-        combos.append(df[((df['Gene_type'] == 'EB_novel'))][["Gene_stable_ID"]])
-
-
+    if (len([col for col in df if col.startswith('SRR')]) or len([col for col in df if col.startswith('ERR')])) >=5:
+        combos.append(df[((df['Gene_type'] == 'EB_novel'))][["TranscriptID"]])
 #Unique list of covid EBs
 covidEBs=pd.concat(combos).drop_duplicates()
-covidEBs.to_csv("covidEBs.txt",sep='\t',index=False,mode='w')
-
+covidEBs.to_csv("SRAcovidEBs.txt",sep='\t',index=False,mode='w')
 
 
 
@@ -30,15 +23,14 @@ covidEBs.to_csv("covidEBs.txt",sep='\t',index=False,mode='w')
 combos=[]
 
 #Files that contain the EB genes that passed each combonation filter.
-files=glob.glob("*_No-Covid_*")
+files=glob.glob("*non_*")
 
 for i in files:
     df=pd.read_csv(i,sep='\t')
     #Sample # must be >= 5 and be EB
-    if (len([col for col in df if col.startswith('batch')])) >=5:
-        combos.append(df[((df['Gene_type'] == 'EB_novel'))][["Gene_stable_ID"]])
-
-
+    if (len([col for col in df if col.startswith('SRR')]) or len([col for col in df if col.startswith('ERR')])) >=5:
+        combos.append(df[((df['Gene_type'] == 'EB_novel'))][["TranscriptID"]])
 #Unique list of non-covid EBs
 nonCovidEBs=pd.concat(combos).drop_duplicates()
-nonCovidEBs.to_csv("nonCovidEBs.txt",sep='\t',index=False,mode='w')
+nonCovidEBs.to_csv("SRAnonCovidEBs.txt",sep='\t',index=False,mode='w')
+
